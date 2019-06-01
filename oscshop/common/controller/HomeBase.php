20cfbd65eddf981f524fe348e5767b28438d4eac
ljf <qukaliujun@163.com>
2019-06-01 16:25:25 +0800

    brand

---
+++ oscshop/common/controller/HomeBase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @author    鱼君瑶
+ *会员中心
+ */
+namespace osc\common\controller;
+use think\Db;
+class HomeBase extends Base{	
+	
+	protected function _initialize() {
+		parent::_initialize();		
+		
+		if(request()->isMobile()&&('mobile'!=request()->module())){
+			header('Location:'.request()->domain().'/mobile/');
+			die();
+		}
+		$this->assign('top_nav',osc_goods()->get_goods_category());
+		$cart=osc_cart();
+		$cart_list=$cart->get_all_goods(member('uid'));		
+		$total_all_price=0;
+		if(isset($cart_list)){
+			foreach ($cart_list as $k => $v) {
+				$total_all_price+=$v['total'];
+			}
+		}
+		$this->assign('cartlist',$cart_list);
+		$this->assign('total_price',$total_all_price);   
+		
+	}
+
+
+
+	
+}
